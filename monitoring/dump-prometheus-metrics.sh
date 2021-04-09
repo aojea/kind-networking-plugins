041559d20610353471214587a974bb958eec9839
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2021-04-09 18:58:50 +0200

    instructions to dump prometheus tar

--- monitoring/dump-prometheus-metrics.sh
+++ monitoring/dump-prometheus-metrics.sh
@@ -7,11 +7,17 @@ docker exec -it kind-control-plane curl -XPOST http://${CLUSTER_IP}:8080/api/v1/
 # get the prometheus database
 POD_NAME=$(kubectl -n monitoring get pods -o jsonpath='{.items[0].metadata.name}')
 kubectl cp monitoring/${POD_NAME}:/prometheus/snapshots /tmp/snapshots
+# or a tarball
+# kubectl -n monitoring exec $POD_NAME -- tar cvf - /prometheus/snapshots > snapshot.tar
 
 # to see the metrics locally
 # create a fake prometheus config so it does not complain
-touch prometheus.yaml
+
+# touch prometheus.yaml
+
 # init the container mounting the folder with the metrics
-docker run --rm -p 9090:9090 -uroot -v /tmp/snapshots:/prometheus prom/prometheus --config prometheus.yml --storage.tsdb.path=/prometheus
+
+# docker run --rm -p 9090:9090 -uroot -v /tmp/snapshots:/prometheus prom/prometheus --config prometheus.yml --storage.tsdb.path=/prometheus
+
 # bonus using grafana (admin:admin) and add prometheus as source
-docker run -d -p 3000:3000 grafana/grafana
+# docker run -d -p 3000:3000 grafana/grafana
