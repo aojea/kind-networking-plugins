1c2bb6d4c2861c3f6b29b5558d43fc8b64a39a44
Antonio Ojea <aojea@redhat.com>
2021-04-10 01:17:43 +0200

    monitoring fixes

--- monitoring/dump-prometheus-metrics.sh
+++ monitoring/dump-prometheus-metrics.sh
@@ -6,9 +6,9 @@ CLUSTER_IP=$(kubectl get svc prometheus-service -n monitoring -o jsonpath="{.spe
 docker exec -it kind-control-plane curl -XPOST http://${CLUSTER_IP}:8080/api/v1/admin/tsdb/snapshot
 # get the prometheus database
 POD_NAME=$(kubectl -n monitoring get pods -o jsonpath='{.items[0].metadata.name}')
-kubectl cp monitoring/${POD_NAME}:/prometheus/snapshots /tmp/snapshots
+#kubectl cp monitoring/${POD_NAME}:/prometheus/snapshots /tmp/snapshots
 # or a tarball
-# kubectl -n monitoring exec $POD_NAME -- tar cvf - /prometheus/snapshots > snapshot.tar
+kubectl -n monitoring exec $POD_NAME -- tar cvf - /prometheus/snapshots > snapshot.tar
 
 # to see the metrics locally
 # create a fake prometheus config so it does not complain
@@ -17,7 +17,10 @@ kubectl cp monitoring/${POD_NAME}:/prometheus/snapshots /tmp/snapshots
 
 # init the container mounting the folder with the metrics
 
-# docker run --rm -p 9090:9090 -uroot -v /tmp/snapshots:/prometheus prom/prometheus --config prometheus.yml --storage.tsdb.path=/prometheus
+#  docker run --rm -p 9090:9090 -uroot \
+#       -v /tmp/prometheus:/prometheus \
+#       -v $PWD/prometheus.yml:/prometheus/prometheus.yml \
+#       prom/prometheus --storage.tsdb.path=/prometheus
 
 # bonus using grafana (admin:admin) and add prometheus as source
 # docker run -d -p 3000:3000 grafana/grafana
